6febbd5758e5c10dd60e22d646e900d73e5eaaf0
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-02-23 16:32:32 -0500

    candidates controller index returns all candidates.

---
+++ test/controllers/candidates_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class CandidatesControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "index returns all candidates" do
+    get :index
+    assert_equal Candidate.all.to_json, response.body
+  end
+
+  test "should get show" do
+    get :show
+    assert_response :success
+  end
+
+  test "should get create" do
+    get :create
+    assert_response :success
+  end
+
+end
